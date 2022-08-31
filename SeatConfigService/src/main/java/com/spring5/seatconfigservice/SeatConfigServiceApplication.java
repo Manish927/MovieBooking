bbcdc1452506a47f8aae12400e11578d8cf38cc4
Manish Srivastava <mannu927@yahoo.com>
2022-08-31 15:57:58 +0530

    payment service

--- SeatConfigService/src/main/java/com/spring5/seatconfigservice/SeatConfigServiceApplication.java
+++ SeatConfigService/src/main/java/com/spring5/seatconfigservice/SeatConfigServiceApplication.java
@@ -2,6 +2,7 @@ package com.spring5.seatconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
